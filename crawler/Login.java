ff4541b9673591cd65fa225867e646e3da17b673
Mengyuan Jing <201321010802@std.uestc.edu.cn>
2016-04-28 14:32:24 +0800

    加入验证码

--- crawler/Login.java
+++ crawler/Login.java
@@ -1,8 +1,19 @@
 package crawler;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -19,9 +30,77 @@ public class Login
         USER_AGENT = _USER_AGENT;
     }
     
+    public String GetCaptcha() throws Exception
+    {
+        String url = "https://www.zhihu.com/captcha.gif";
+        
+        URL obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+
+        int res_code = conn.getResponseCode();
+        System.out.println("\nGET:" + url + ":" + res_code);
+        
+        DataInputStream in = new DataInputStream(conn.getInputStream());
+
+        byte[] bytes = new byte[1024];
+        byte[] content = new byte[65536];
+        int i = in.read(bytes);
+        int j = 0;
+        while (i > 0)
+        {
+            for (int j2 = j, i2 = 0; j2 < j+i && i2 < i; j2++, i2++)
+            {
+                content[j2] = bytes[i2];
+            }
+            
+            j += i;
+            i = in.read(bytes);
+        }
+        
+
+        File file = new File("c:\\captcha.gif");
+        if (file.exists())
+        {
+            System.out.println(file.delete());
+        }
+        file.createNewFile();
+        
+        DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
+        
+        for (int j2 = 0; j2 < j; j2++)
+        {
+            out.writeByte(content[j2]);
+            out.flush();
+        }     
+        out.close();
+        
+        Runtime.getRuntime().exec("rundll32 c:\\Windows\\System32\\shimgvw.dll,ImageView_Fullscreen c:\\captcha.gif");
+        
+        System.out.print("请输入验证码：");
+        Scanner scan = new Scanner(System.in);
+        String code = scan.nextLine();
+
+        return code;
+    }
+    
+    public String GetXSRF() throws Exception
+    {
+        String url = "https://www.zhihu.com/";
+        
+        URL obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        
+        return url;
+    }
+    
     // POST登录信息
     public boolean SendPost(String url, String post_params) throws Exception
     {
+        String code = GetCaptcha();
         URL obj = new URL(url);
         conn = (HttpsURLConnection)obj.openConnection();
         
@@ -44,6 +123,10 @@ public class Login
 
         if (responseCode != 200)
             return false;
+        
+        String check_url = "https://www.zhihu.com/settings/profile";
+        
+        Crawler.GetPageContent(check_url, conn);
         
         // 获取cookies
         cookies = conn.getHeaderFields().get("Set-Cookie");
